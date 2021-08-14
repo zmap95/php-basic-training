6b977e6cfc354aae7382899558d9dae366451c99
canhht <canhht@scuti.asia>
2021-08-14 14:39:57 +0700

    Update content for assignment operator

---
+++ lession-1/assignment_operators.php
@@ -0,0 +1,36 @@
+<?php
+
+$firstNumber = 8;
+$secondNumber = 4;
+
+// Addition
+$firstNumber += $secondNumber;
+echo 'The value of first number is ' . $firstNumber . PHP_EOL . PHP_EOL; 
+
+$firstNumber = 8;
+$secondNumber = 4;
+
+// Subtraction
+$firstNumber -= $secondNumber;
+echo 'The value of first number is ' . $firstNumber . PHP_EOL . PHP_EOL; 
+
+$firstNumber = 8;
+$secondNumber = 4;
+
+// Multiplication
+$firstNumber *= $secondNumber;
+echo 'The value of first number is ' . $firstNumber . PHP_EOL . PHP_EOL; 
+
+$firstNumber = 8;
+$secondNumber = 4;
+
+// Division
+$firstNumber /= $secondNumber;
+echo 'The value of first number is ' . $firstNumber . PHP_EOL . PHP_EOL; 
+
+$firstNumber = 8;
+$secondNumber = 4;
+
+// Modulus
+$firstNumber %= $secondNumber;
+echo 'The value of first number is ' . $firstNumber . PHP_EOL . PHP_EOL;
