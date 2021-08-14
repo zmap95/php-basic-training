fca9a295f52335563fa1cb73bfe567f91d51770e
canhht <canhht@scuti.asia>
2021-08-14 11:49:02 +0700

    Add arithmetic operator

---
+++ lession-1/arithmetic_operators.php
@@ -0,0 +1,28 @@
+<?
+
+$firstNumber = 8;
+$secondNumber = 4;
+
+// Addition
+$sum = $firstNumber + $secondNumber;
+echo 'Sum of $firstNumber and $secondNumber is ' . $sum . PHP_EOL; 
+
+// Subtraction
+$difference = $firstNumber - $secondNumber;
+echo 'Difference of $firstNumber and $secondNumber is ' . $difference . PHP_EOL; 
+
+// Multiplication
+$product = $firstNumber * $secondNumber;
+echo 'Product of $firstNumber and $secondNumber is : ' . $product . PHP_EOL; 
+
+// Division
+$quotient = $firstNumber / $secondNumber;
+echo 'Quotient of $firstNumber and $secondNumber is : ' . $quotient . PHP_EOL; 
+
+// Modulus
+$remainder = $firstNumber % $secondNumber;
+echo 'Remainder of $firstNumber and $secondNumber is : ' . $remainder . PHP_EOL;
+
+// Exponentiation
+$exponentiation = $firstNumber ** $secondNumber;
+echo 'Result of raising $firstNumber to the $secondNumber\'th power is : ' . $exponentiation . PHP_EOL;
