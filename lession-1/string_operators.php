4534c32d37ca1fd4af1b9d6c113060a843bccbd6
Canh Hoang <hoangcanhit95@gmail.com>
2021-08-24 23:35:29 +0700

    Add string operators

---
+++ lession-1/string_operators.php
@@ -0,0 +1,12 @@
+<?php
+// Operator '.='
+$txt1 = "Hello";
+$txt2 = " world!";
+$txt1 .= $txt2;
+echo "Giá trị của biến \$txt1 là : " . $txt1 . "<br>";
+
+// Operator '.'
+$txt1 = "Hello";
+$txt2 = " world!";
+
+echo "Giá trị của \$txt1 và \$txt2 là : " . $txt1 . $txt2;
